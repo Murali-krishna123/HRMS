b9cd8b0fa27381be1752ff2badf580f9d582389d
Murali-krishna123 <tumuradamuralikrishna91@gmail.com>
2024-11-22 12:21:38 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { Dashboard1Component } from './dashboard1/dashboard1.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { AttendencereportComponent } from './attendencereport/attendencereport.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    CalendarComponent,
+    Dashboard1Component,
+    MyaccountComponent,
+    AttendencereportComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,FormsModule,ReactiveFormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
